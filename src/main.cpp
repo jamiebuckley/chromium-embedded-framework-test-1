5ba56d9cabaac0954ff30466d930f55748057798
Jamie Buckley <jbuckley760@gmail.com>
2019-06-03 21:45:02 +0100

    Initial commit working with CEF

---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include "include/cef_app.h"
+#include "simple_handler.h"
+#include <iostream>
+#include <include/views/cef_browser_view.h>
+#include <include/views/cef_window.h>
+#include <include/wrapper/cef_library_loader.h>
+#include "simple_app.h"
+
+// When using the Views framework this object provides the delegate
+// implementation for the CefWindow that hosts the Views-based browser.
+class SimpleWindowDelegate : public CefWindowDelegate {
+public:
+    explicit SimpleWindowDelegate(CefRefPtr<CefBrowserView> browser_view)
+            : browser_view_(browser_view) {}
+
+    void OnWindowCreated(CefRefPtr<CefWindow> window) OVERRIDE {
+        // Add the browser view and show the window.
+        window->AddChildView(browser_view_);
+        window->Show();
+
+        // Give keyboard focus to the browser view.
+        browser_view_->RequestFocus();
+    }
+
+    void OnWindowDestroyed(CefRefPtr<CefWindow> window) OVERRIDE {
+        browser_view_ = NULL;
+    }
+
+    bool CanClose(CefRefPtr<CefWindow> window) OVERRIDE {
+        // Allow the window to close if the browser says it's OK.
+        CefRefPtr<CefBrowser> browser = browser_view_->GetBrowser();
+        if (browser)
+            return browser->GetHost()->TryCloseBrowser();
+        return true;
+    }
+
+private:
+    CefRefPtr<CefBrowserView> browser_view_;
+
+IMPLEMENT_REFCOUNTING(SimpleWindowDelegate);
+    DISALLOW_COPY_AND_ASSIGN(SimpleWindowDelegate);
+};
+
+
+int main(int argc, char** argv) {
+    CefScopedLibraryLoader library_loader;
+    if (!library_loader.LoadInMain())
+        return 1;
+
+    // Provide CEF with command-line arguments.
+    CefMainArgs main_args(argc, argv);
+
+    std::cout << "Hello, World2!" << std::endl;
+
+    CefBrowserSettings cefBrowserSettings;
+
+    // Specify CEF global settings here.
+    CefSettings settings;
+
+    // SimpleApp implements application-level callbacks for the browser process.
+    // It will create the first browser instance in OnContextInitialized() after
+    // CEF has initialized.
+
+    CefRefPtr<SimpleApp> app(new SimpleApp);
+
+    // Initialize CEF for the browser process.
+    CefInitialize(main_args, settings, app.get(), NULL);
+
+    std::cout << "Starting loop" << std::endl;
+
+    // Run the CEF message loop. This will block until CefQuitMessageLoop() is
+    // called.
+    CefRunMessageLoop();
+
+    std::cout << "Shutting down!" << std::endl;
+    // Shut down CEF.
+    CefShutdown();
+    return 0;
+}
